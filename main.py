fb2b56abaca7eb005dd5d18e6305df9890940a9a
Horacio PÃerez <horaciorodri13@gmail.com>
2020-09-22 18:21:17 -0500

    Seimplementa la extraccion de datos de los articulos

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import argparse
 import logging
 logging.basicConfig(level=logging.INFO)
+import re
+#is_well_formed_link=re.compile(r'^https?://.+/.$)
+#is_root_path= re.compile(r'^/.+$')
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
 
 import news_page_objects as news
 from common import config
 
 logger = logging.getLogger(__name__)
+
+is_well_formed_link = re.compile(r'^https?://.+/.+$') 
+is_root_path = re.compile(r'^/.+$')    
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
@@ -13,8 +21,41 @@ def _news_scraper(news_site_uid):
     logging.info(f'Beginning scrape for {host}')
     homepage = news.HomePage(news_site_uid, host)
 
+    articles = []
     for link in homepage.article_links:
-        print(link)
+        #print(link)
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info('Article fetched!!')
+            articles.append(article)
+            print(article.title)
+
+    print(len(articles))
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info(f'Start fetching article at {link}')
+
+    article = None
+
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article', exc_info=False)
+
+    if article and not article.body:
+        logger.warning('Invalid article. There is no body')
+        return None
+
+    return article
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return f'{host}{link}'
+    else:
+        return f'{host}/{link}'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- news_page_objects.py
+++ news_page_objects.py
@@ -3,20 +3,31 @@ import requests
 
 from common import config
 
-class HomePage:
+
+class NewsPage:
 
     def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
-        self._html =None
+        self._html = None
 
         self._visit(url)
-    
-    def _visit(self,url):
+
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
+
+    def _visit(self, url):
         response = requests.get(url)
 
         response.raise_for_status()
-        self._html = bs4.BeautifulSoup(response.text,'html.parser')
+
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
 
     @property
     def article_links(self):
@@ -24,8 +35,20 @@ class HomePage:
         for link in self._select(self._queries['homepage_article_links']):
             if link and link.has_attr('href'):
                 link_list.append(link)
-
-        return set(link['href']for link in link_list)
         
-    def _select(self, query_string):
-        return self._html.select(query_string)
+        return set(link['href'] for link in link_list)
+
+class ArticlePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
